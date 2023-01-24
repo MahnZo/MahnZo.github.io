7823b00a18b831bc599091381bb5f05e66d75c59
MahnZo <69412142+MahnZo@users.noreply.github.com>
2023-01-23 20:20:02 -0800

    linked js and css to html
    linked the js , css files to the html with a bit of starter code

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Get a reference to a button element on the page
+var button = document.getElementById("myButton");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+  // Code to be executed when the button is clicked
+  alert("Button was clicked!");
+});
 
