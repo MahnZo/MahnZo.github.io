ca4571a2bacc17140dc2a12dceaadb39fd164f05
MahnZo <69412142+MahnZo@users.noreply.github.com>
2023-01-30 15:46:52 -0800

    update to version num andded

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 // add a dynamic header
-let versionNum = "1.0";
+let versionNum = "1.1";
 document.getElementById("header").innerHTML = "Welcome to my website! " + versionNum;
 
 
-// Get a reference to a button element on the page
+    // Get a reference to a button element on the page
 var button = document.getElementById("myButton");
 
-// Add a click event listener to the button
+    // Add a click event listener to the button
 button.addEventListener("click", function() {
-  // Code to be executed when the button is clicked
-  alert("Button was clicked!");
-});
+    // Code to be executed when the button is clicked
+    alert("Button was clicked!");
+    });
 
 
 function changeColor() {
-  var color = document.getElementById("color").value;
-  document.body.style.background = color;
+    var color = document.getElementById("color").value;
+    document.body.style.background = color;
+}
+
+function resetColor() {
+    document.body.style.background = lightblue;
 }
