8580423aeb0b3efdd32a10f6d522d0e9f59c0d7d
MahnZo <69412142+MahnZo@users.noreply.github.com>
2023-01-30 20:45:30 -0800

    update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 // add a dynamic header
-let versionNum = "1.63";
+let versionNum = "1.67";
 document.getElementById("header").innerHTML = "Welcome to my website! " + versionNum;
 
 
     // Get a reference to a button element on the page
-var button = document.getElementById("mySelect");
+`var button = document.getElementById("mySelect");
 
     // Add a click event listener to the button
 button.addEventListener("click", function() {
     // Code to be executed when the button is clicked
     alert("Button was clicked!");
     });
+`
+
 
 
 function changeColor() {
